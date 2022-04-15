da53511bf6dea7cd904f0c7820977f32a565f29d
Alex <alexkamm61@gmail.com>
2022-04-15 13:49:29 +1000

    start of calculator

---
+++ calculator.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+ROOT = Tk()
+ROOT.geometry("312x324")
+ROOT.title("Calculator")
+
+myLabel = Label(ROOT, text="Hello World!")
+myLabel.pack()
+
+ROOT.mainloop()
+
+#? what is this: E = Entry(ROOT, width = 50)
