e86648b503f361571bee7e68b4a550d8d1f60e8a
alex-k-exe <alexk61@duck.com>
2024-11-16 21:58:25 +1000

    Shadcn is working

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,34 @@
 <script lang="ts">
-	import '../app.css';
-	let { children } = $props();
+import { Moon, Sun } from "lucide-svelte";
+import { ModeWatcher } from "mode-watcher";
+import "../app.pcss";
+
+import { buttonVariants } from "$lib/components/ui/button/index.js";
+import * as DropdownMenu from "$lib/components/ui/dropdown-menu/index.js";
+import { resetMode, setMode } from "mode-watcher";
+
+let { children } = $props();
 </script>
 
+<h1>Test</h1>
+<ModeWatcher />
+<DropdownMenu.Root>
+  <DropdownMenu.Trigger
+    class={buttonVariants({ variant: "outline", size: "icon" })}
+  >
+    <Sun
+      class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+    />
+    <Moon
+      class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+    />
+    <span class="sr-only">Toggle theme</span>
+  </DropdownMenu.Trigger>
+  <DropdownMenu.Content align="end">
+    <DropdownMenu.Item onclick={() => setMode("light")}>Light</DropdownMenu.Item
+    >
+    <DropdownMenu.Item onclick={() => setMode("dark")}>Dark</DropdownMenu.Item>
+    <DropdownMenu.Item onclick={() => resetMode()}>System</DropdownMenu.Item>
+  </DropdownMenu.Content>
+</DropdownMenu.Root>
 {@render children()}
