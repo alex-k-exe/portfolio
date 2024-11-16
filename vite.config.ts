682e299dcb4ba56d662682c35240f12ce9dc39d1
alex-k-exe <alexk61@duck.com>
2024-11-16 20:44:54 +1000

    Haven't installed Tailwind?

--- vite.config.ts
+++ vite.config.ts
@@ -3,21 +3,9 @@ import { enhancedImages } from "@sveltejs/enhanced-img";
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vitest/config";
 
-import { esbuildCommonjs } from "@originjs/vite-plugin-commonjs";
-
 export default defineConfig({
   plugins: [sveltekit(), viteCommonjs(), enhancedImages()],
   test: {
     include: ["src/**/*.{test,spec}.{js,ts}"],
-  },
-  ssr: {
-    noExternal: ["dayjs"],
-  },
-
-  optimizeDeps: {
-    esbuildOptions: {
-      plugins: [esbuildCommonjs(["dayjs"])],
-    },
-    include: ["dayjs"],
   },
 });
