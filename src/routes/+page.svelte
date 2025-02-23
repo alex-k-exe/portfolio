977d8c030b8f84feecf7fc81c50b39589a8ed19a
Alex Kammin <alexk61@duck.com>
2025-02-23 14:42:54 +1000

    working with svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
+<script lang="ts">
+	let projects = ['Given Example', 'Three Squares'];
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-
-<iframe src={`./nannou-embed?id=1`} title="Example"></iframe>
-<iframe src={`./nannou-embed?id=2`} title="Three squares"> </iframe>
--- src/routes/nannou-embed/+page.svelte
+++ src/routes/nannou-embed/+page.svelte
@@ -1,11 +1,21 @@
 <script lang="ts">
-	let { id: string } = $props();
+	import { onMount } from 'svelte';
 
-	await import(`../../crates/nannou-${id}/pkg/nannou_web_test.js`).then(
-		async ({ default: nannou }) => {
-			await nannou().then((s: { main_web: () => void }) => {
-				s.main_web();
-			});
-		}
-	);
+	let { data } = $props();
+
+	console.log(data.id);
+
+	async function loadNannou() {
+		await import(`$lib/crates/nannou-${data.id}/pkg/nannou_web_test.js`).then(
+			async ({ default: nannou }) => {
+				await nannou().then((s: { main_web: () => void }) => {
+					console.log(`aaaeee ${data.id}`);
+					s.main_web();
+				});
+			}
+		);
+	}
+
+	// Call the function when the component mounts
+	onMount(loadNannou);
 </script>
